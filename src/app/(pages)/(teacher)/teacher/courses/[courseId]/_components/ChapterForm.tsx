a9d2633289f75cda0007be608520180f8f132e27
sudhachandrann <sudhachandran1020@gmail.com>
2025-02-27 00:39:55 +0530

    add chapterlist

--- src/app/(pages)/(teacher)/teacher/courses/[courseId]/_components/ChapterForm.tsx
+++ src/app/(pages)/(teacher)/teacher/courses/[courseId]/_components/ChapterForm.tsx
@@ -56,7 +56,7 @@ function ChapterForm({ initialData, courseId }: ChapterFormProps) {
   };
 
   return (
-    <div className="w-[380px] md:w-[450px] p-4 px-6 bg-slate-100 rounded-md">
+    <div className="w-full lg:w-4/5 min-w-[320px]  p-3 md:p-4  bg-slate-100 rounded-md  mb-4">
       <div className="font-medium flex items-center justify-between">
         <span>Course Chapters</span>
         <Button
