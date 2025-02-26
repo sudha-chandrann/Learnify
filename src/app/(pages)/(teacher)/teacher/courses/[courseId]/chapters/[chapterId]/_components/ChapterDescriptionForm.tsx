d53df25c31955a9cdf41e5c906df48d1a6cb6af6
sudhachandrann <sudhachandran1020@gmail.com>
2025-02-27 01:48:59 +0530

    edit chapterdescriptionform

--- src/app/(pages)/(teacher)/teacher/courses/[courseId]/chapters/[chapterId]/_components/ChapterDescriptionForm.tsx
+++ src/app/(pages)/(teacher)/teacher/courses/[courseId]/chapters/[chapterId]/_components/ChapterDescriptionForm.tsx
@@ -1,15 +1,23 @@
 "use client";
 import React, { useState } from "react";
 import * as z from "zod";
-
+import axios from "axios";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
 import { Pencil } from "lucide-react";
-
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
 import { Chapter } from "@prisma/client";
-
+import Preview from "@/components/customui/Preview";
+import RichTextEditor from "@/components/customui/TextEditor";
 
 interface DescriptionFormProps {
   initialData: Chapter;
@@ -35,7 +43,23 @@ function ChapterDescriptionForm({
 
   const { isSubmitting, isValid } = form.formState;
   const [isEditing, setIsEditing] = useState(false);
-  console.log(chapterId,courseId,isValid);
+  const router = useRouter();
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      await axios.patch(
+        `/api/courses/${courseId}/chapters/${chapterId}`,
+        values
+      );
+      toast.success("Description updated successfully!");
+      setIsEditing(false);
+      router.refresh();
+
+    } catch (error) {
+      console.error("Error updating Description:", error);
+      toast.error("Something went wrong. Please try again.");
+    }
+  };
 
   const toggleEdit = () => setIsEditing((current) => !current);
 
@@ -54,12 +78,63 @@ function ChapterDescriptionForm({
         </Button>
       </div>
 
-
+      {!isEditing && initialData.description && (
+       <Preview content={initialData.description}/>
+      )}
       {!isEditing && !initialData.description && (
         <p className="text-sm mt-2 text-slate-700 italic">No description</p>
       )}
 
+      {isEditing && (
+        <div className="bg-black/45 fixed top-0 left-0 z-50 h-screen w-screen flex items-center justify-center">
+            <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-4 mt-4 w-full lg:w-3/5 mx-auto"
+          >
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <RichTextEditor
+                     content={initialData.description}
+                     onChange={(value) =>
+                      field.onChange(value)
+                     }
+                     />
 
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex items-center gap-x-2 justify-between">
+              <Button
+                type="submit"
+                disabled={isSubmitting || !isValid}
+                variant="teacher"
+                size="sm"
+              >
+                {isSubmitting ? "Saving..." : "Save Description"}
+              </Button>
+              <Button
+                type="submit"
+                disabled={isSubmitting || !isValid}
+                variant="teacher"
+                size="sm"
+                onClick={()=>{setIsEditing(false)}}
+              >
+                 Cancal
+              </Button>
+            </div>
+          </form>
+        </Form>
+        </div>
+     
+      )}
+      
     </div>
   );
 }
