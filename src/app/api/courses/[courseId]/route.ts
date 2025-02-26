5a2ff82b490cf9b2282ad4e61f784f873178b5ca
sudhachandrann <sudhachandran1020@gmail.com>
2025-02-27 02:27:08 +0530

    update course update api

--- src/app/api/courses/[courseId]/route.ts
+++ src/app/api/courses/[courseId]/route.ts
@@ -1,6 +1,13 @@
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 export async function PATCH(
     req: Request,
@@ -18,7 +25,29 @@ export async function PATCH(
       }
   
       const values = await req.json();
-  
+      if(values.imageUrl){
+        const course=await db.course.findUnique({
+            where: { id: courseId,userId },
+        })
+        if(!course){
+            return NextResponse.json({ error: "course not found" }, { status: 404 });
+        }
+        if(course.imageUrl){
+            try {
+                const publicId = course.imageUrl.split("/").pop()?.split(".")[0];
+                if (publicId) {
+                  await cloudinary.uploader.destroy(publicId);
+                }
+              } catch (error) {
+                console.error(`Failed to delete course Image ${course.imageUrl} from Cloudinary`, error);
+                return NextResponse.json(
+                  { error: "Internal Server Error" },
+                  { status: 500 }
+                );
+            }
+        }
+
+      }
       const updatedCourse = await db.course.update({
         where: {
           id: courseId,
