e238cab62c6d0aa71d6f7bf135ccba529ac00dbd
sudhachandrann <sudhachandran1020@gmail.com>
2025-02-26 17:33:38 +0530

    add create attachment api

---
+++ src/app/api/courses/[courseId]/attachments/route.ts
@@ -0,0 +1,60 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function POST(
+  req: Request,
+  { params }: { params: Promise<{ courseId: string }> }
+) {
+  try {
+    const { userId } = await auth();
+    const {courseId}= await params;
+
+    if (!userId) {
+      return NextResponse.json(
+        { error: "Unauthorized access" },
+        { status: 401 }
+      );
+    }
+
+    const { url ,name} = await req.json();
+
+    // Check if the course belongs to the user
+    const courseOwner = await db.course.findUnique({
+      where: {
+        id: courseId,
+        userId: userId, // Ensure the course belongs to the current user
+      },
+    });
+
+    if (!courseOwner) {
+      return NextResponse.json(
+        { error: "Unauthorized access to this course" },
+        { status: 401 }
+      );
+    }
+
+    // Create the attachment
+    const attachment = await db.attachment.create({
+      data: {
+        url,
+        name: name || "Unnamed Attachment",
+        courseId: courseId,
+      },
+    });
+    if(!attachment){
+        return NextResponse.json(
+            { error: "Failed to create attachment" },
+            { status: 500 }
+        );
+    }
+
+    return NextResponse.json(attachment, { status: 200 });
+  } catch (error) {
+    console.error("[COURSE_ID_ATTACHMENT_ERROR]", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
